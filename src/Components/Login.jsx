4e367bb48c5f87c3f5c6a216c87dced9111a8924
abhishek <abhishekshrivastav1920@gmail.com>
2022-05-21 22:04:22 +0530

    added login functioning

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import "./Login.css";
-import {auth} from "../firebase";
-import  {signInWithEmailAndPassword} from 'firebase/auth'
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 function Login() {
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
 
-  let [user,setUser] = useState(null);
-  let [loader,setLoader] = useState(false);
-  let [error,setError] = useState(null);
-
+  let [user, setUser] = useState(null);
+  let [loader, setLoader] = useState(false);
+  let [error, setError] = useState('');
 
   const changeEmail = (e) => {
     console.log(e.target.value);
@@ -20,48 +19,57 @@ function Login() {
     setPassword(e.target.value);
   };
 
-    const printCred = async function(){
-    setLoader(true);
-    let userCred = await 
-    signInWithEmailAndPassword(auth,email,password);
-    console.log(userCred.user.uid);
-    setUser(userCred.user)
-        setLoader(false);
-
-
-
-    };
-  return (
-    <>{loader === true ? <h1> Loading....</h1> : 
-
-    user != null ? <h1>user is {user.uid}</h1> : 
-
-
-    <>
-      <div className="flex-cotainer">
-        <span> Email : </span>
-        <input
-          value={email}
-          placeholder="email"
-          onChange={changeEmail}
-          type="email"
-        ></input>
-      </div>
-      <div className="flex-cotainer">
-        <span> Password : </span>
-        <input
-          value={password}
-          placeholder="password"
-          onChange={changePassword}
-          type="password"
-        ></input>
-      </div>
-
-      <button type="submit" onClick={printCred}>
-        {" "}
-        Submit
-      </button></>
+  const printCred = async function () {
+    try {
+      setLoader(true);
+      let userCred = await signInWithEmailAndPassword(auth, email, password);
+      console.log(userCred.user.uid);
+      setUser(userCred.user);
+    } catch (e) {
+      setError(e.message);
     }
+    setTimeout(() => {
+        setError(""); 
+    }, 2000);
+    setLoader(false);
+
+    // after some time  => error message removed
+  };
+
+  console.log(error);
+  return (
+    <>
+      {error!==''? <h1> Error is {error}</h1> : loader === true ? (
+        <h1> Loading....</h1>
+      ) : user != null ? (
+        <h1>user is {user.uid}</h1>
+      ) : (
+        <>
+          <div className="flex-cotainer">
+            <span> Email : </span>
+            <input
+              value={email}
+              placeholder="email"
+              onChange={changeEmail}
+              type="email"
+            ></input>
+          </div>
+          <div className="flex-cotainer">
+            <span> Password : </span>
+            <input
+              value={password}
+              placeholder="password"
+              onChange={changePassword}
+              type="password"
+            ></input>
+          </div>
+
+          <button type="submit" onClick={printCred}>
+            {" "}
+            Submit
+          </button>
+        </>
+      )}
     </>
   );
 }
