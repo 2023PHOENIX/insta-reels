5d5c1fde12ba34932db9df030f1fdd036bd387cd
abhishek <abhishekshrivastav1920@gmail.com>
2022-05-29 12:51:03 +0530

    added upto profile.js

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -15,58 +15,54 @@ TODO:
  */
 function Profile() {
   let userObj = useContext(AuthContext);
-  // let [loading,setLoading] = useState(false);
+  let [loading,setLoading] = useState(false);
   // console.log(userObj);
 
-  useEffect(function fn() {
+  // useEffect(function fn() {
  
-    (async function(){
+  //   (async function(){
 
-      const docRef = doc(db,'user',userObj.cUser.uid);
-      const docSnap = await getDoc(docRef);
+  //     const docRef = doc(db,'user',userObj.cUser.uid);
+  //     const docSnap = await getDoc(docRef);
   
   
-      if(docSnap.exists()){
-        console.log("document is ",docSnap.data())
-      }
+  //     if(docSnap.exists()){
+  //       console.log("document is ",docSnap.data())
+  //     }
    
-    })()
+  //   })()
     
-  })() , [userObj.cUser]); 
+  // })() , [userObj.cUser]); 
 
   return (
-    <div>
+  <>
 
 
-      {userObj.cUser===null ? <div> user is not valid</div> : <div> user is {userObj.cUser.uid}</div>}
-  
-    </div>
+     { userObj.cUser===null ? <div> user is not valid</div> : 
+      
+      loading===true ? <div> Loading getting data  ...</div> : 
+    (<>
+      <div className="header"></div>
+      <div className="main">
+        <div className="profile-image-container">
+
+        <img src="https://www.fillmurray.com/640/360" className="profile-img" alt="##"></img>
+        </div>
 
 
-
-    // loading===true? <div> Loading getting data  ...</div> : 
-    // <>
-    //   <div className="header"></div>
-    //   <div className="main">
-    //     <div className="profile-image-container">
-
-    //     <img src="https://www.fillmurray.com/640/360" className="profile-img" alt="##"></img>
-    //     </div>
-
-
-    //     <div className="details">
-    //       <div className="content">Name</div>
-    //       <div className="content">
-    //         No of posts : <span className="bold-text">posts</span>
-    //       </div>
-    //       <div className="content">
-    //         Email : <span className="bold-text"> Email.com</span>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </>
-  
+        <div className="details">
+          <div className="content">Name</div>
+          <div className="content">
+            No of posts : <span className="bold-text">posts</span>
+          </div>
+          <div className="content">
+            Email : <span className="bold-text"> Email.com</span>
+          </div>
+        </div>
+      </div>
+    </>)}
+      
+  </>
   );
 }
-
 export default Profile;
