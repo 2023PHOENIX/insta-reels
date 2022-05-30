ff96184f58a587c1f639628ae01db68e0a694f17
abhishek <abhishekshrivastav1920@gmail.com>
2022-05-30 15:25:28 +0530

    private Routes not working properly static feed ui is partially built

--- src/App.js
+++ src/App.js
@@ -1,51 +1,87 @@
-import './App.css';
-import Feed from './Components/Feed';
-import Login from './Components/Login';
-import PageNotFound from './Components/PageNotFound';
-import Profile from './Components/Profile';
-import Signup from './Components/Signup';
+// eslint-disable-next-lin
+import "./App.css";
+import Feed from "./Components/Feed";
+import Login from "./Components/Login";
+import PageNotFound from "./Components/PageNotFound";
+import Profile from "./Components/Profile";
+import Signup from "./Components/Signup";
 
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-} from 'react-router-dom';
-import { AuthContextProvider } from './Context/AuthContext';
-
-
-
+  Route,  Redirect,
+} from "react-router-dom";
+import { AuthContext, AuthContextProvider } from "./Context/AuthContext";
+import React, { useContext } from "react";
 
 function App() {
   return (
     <AuthContextProvider>
       <Router>
-        <Switch> 
+        <Switch>
+          <RedirectToFeed path="/login" comp={Login}></RedirectToFeed>
+          <RedirectToFeed path="/signup" comp={Signup}></RedirectToFeed>
 
-          <Route path = "/feed"> 
-            <Feed/>
+          <PrivateRoute path="/feed" comp={Feed}></PrivateRoute>
+          <PrivateRoute path="/profile" comp={Profile}></PrivateRoute>
+
+          {/* <Route path="/Feed">
+            <Feed />
+          </Route> */}
+          {/* <Route path="/profile">
+            <Profile />
+          </Route> */}
+
+          <Route path="/" exact>
+            <Feed />
           </Route>
-          
-          <Route path = "/Login"> 
-            <Login/>
+          <Route>
+            <PageNotFound />
           </Route>
-          <Route path = "/Profile"> 
-            <Profile/>
-          </Route>
-          <Route path = "/signup"> 
-            <Signup/>
-          </Route>
-          <Route path = "/" exact> 
-            <Feed/>
-          </Route>
-          <Route> 
-            <PageNotFound/>
-          </Route>
-          
         </Switch>
-
       </Router>
     </AuthContextProvider>
-    
+  );
+}
+
+function PrivateRoute(props) {
+  // console.log("private Route", props);
+  let Component = props.comp;
+  // check -> are you loggedIN
+  let cUser = useContext(AuthContext);
+  console.log(cUser);
+  // cuser-> null ->logi page
+  // cuser-> anything
+  return (
+    <Route
+      {...props}
+      render={(props) => {
+        // logic
+        return cUser !== null ? (
+          <Component {...props}></Component>
+        ) : (
+          <Redirect {...props} to="/login"></Redirect>
+        );
+      }}
+    ></Route>
+  );
+}
+
+function RedirectToFeed(props) {
+  let Component = props.comp;
+  let cUser = useContext(AuthContext);
+  console.log(cUser);
+  return (
+    <Route
+      {...props}
+      render={(props) => {
+        return cUser ? (
+          <Redirect {...props} to="/feed"></Redirect>
+        ) : (
+          <Component {...props}></Component>
+        );
+      }}
+    ></Route>
   );
 }
 
--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import instaIcon from "../Images/Insta.png";
+import profile from "../Images/mine.png";
+import upload from "../Images/icons8-movie-48.png"
 
+import './Feed.css';
 function Feed() {
   return (
-    <div>Feed</div>
+    <>
+      <div className='header'>
+      <img src = {instaIcon} alt="#" className='instaIcon'/>
+        <img src = {profile} alt = "#" className='profileImage'/>
+      </div>
+
+      <br></br>
+
+      <div className='main_container'>
+      <button className='upload_container'>
+          <img src = {upload} className = {upload}alt="#"/>
+          <span>UPLOAD</span>
+      </button>
+      <div></div>
+      <div className='reels_container'>Reels</div>
+      </div>
+    </>
   )
 }
 
